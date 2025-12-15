f6e962efdb2acf323fcd8f249ce0a40360a99f50
Md Saiful Islam Rimon <saifulofficial025@gmail.com>
2025-12-15 15:11:26 +0600

    initial commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h2 className="text-3xl font-bold">landing page contain goes here</h2>
       <Container>
-          <h2 className="text-3xl font-bold">Container Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt animi alias distinctio eligendi doloremque ducimus illum? Sint harum expedita quibusdam voluptate nihil sed porro quam provident! Vitae numquam delectus quasi.</h2>
+          <h2 className="text-3xl ">Container Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt animi alias distinctio eligendi doloremque ducimus illum? Sint harum expedita quibusdam voluptate nihil sed porro quam provident! Vitae numquam delectus quasi.</h2>
       </Container>
     </div>
   )
